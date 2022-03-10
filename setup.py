90401bbed4d8fc5a62eb874488ac72c44d258497
Nate <minecraftcrusher100@gmail.com>
2022-03-10 11:24:16 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,16 @@ def readme():
     with open('Readme.rst', 'r') as filename:
         return filename.read()
 setup(
-    name='sphinx_execute_code',
+    name='sphinx_execute_code_python3',
     version='0.2a2',
     platforms=['any'],
     packages=['sphinx_execute_code'],
-    url='https://github.com/jpsenior/sphinx-execute-code',
+    url='https://github.com/GrandMoff100/sphinx-execute-code',
     license='MIT',
     author='JP Senior',
     author_email='jp.senior@gmail.com',
+    maintainer="GrandMoff100",
+    maintainer_email="nlarsen23.student@gmail.com",
     description='Sphinx support for execution of python code from code blocks or files',
     long_description=readme(),
     install_requires=['docutils', 'sphinx'],
@@ -25,6 +27,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        
     ],
     keywords='sphinx extension directive execute code',
 )
